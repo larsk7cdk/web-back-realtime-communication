c6cd733552676b0a38e46be99709a00611a0a9ed
Lars Larsen <lal@larss-mbp.home>
2019-03-10 19:56:33 +0100

    initial commit

---
+++ web-back-realtime-communication.ws/Extensions/WebsocketEchoMiddlewareExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using web_back_realtime_communication.ws.Middlewares;
+
+namespace web_back_realtime_communication.ws.Extensions
+{
+    public static class WebsocketEchoMiddlewareExtension
+    {
+        public static IApplicationBuilder UseWebsocketEchoMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<WebsocketEchoMiddleware>();
+        }
+    }
+}
---
+++ web-back-realtime-communication.ws/Middlewares/WebsocketEchoMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace web_back_realtime_communication.ws.Middlewares
+{
+    public class WebsocketEchoMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public WebsocketEchoMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+
+            if (context.Request.Path == "/ws")
+            {
+                if (context.WebSockets.IsWebSocketRequest)
+                {
+                    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    await Echo(context, webSocket);
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                }
+            }
+            else
+            {
+                await _next?.Invoke(context);
+            }
+
+        }
+
+
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+}
