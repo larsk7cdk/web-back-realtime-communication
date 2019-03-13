c800b5d9ba2e1878c669330498dbf6f5e3989bba
Lars Larsen <lal@jyskebank.dk>
2019-03-13 12:24:22 +0100

    fixed chat service

--- web-back-realtime-communication-ng/src/app/app.component.ts
+++ web-back-realtime-communication-ng/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ChatService } from './services/chat.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'web-back-realtime-communication-ng';
+    title = 'web-back-realtime-communication-ng';
+
+    constructor(private chatService: ChatService) {
+        chatService.messages.subscribe(msg => {
+            console.log('Response from websocket: ' + msg);
+        });
+    }
+
+    private message = {
+        author: 'tutorialedge',
+        message: 'this is a test message',
+    };
+
+    sendMsg() {
+        console.log('new message from client to websocket: ', this.message);
+        this.chatService.messages.next(this.message);
+        this.message.message = '';
+    }
 }
--- web-back-realtime-communication-ng/src/app/services/chat.service.ts
+++ web-back-realtime-communication-ng/src/app/services/chat.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Message } from '@angular/compiler/src/i18n/i18n_ast';
-import {pipe, Subject} from 'rxjs';
+import { Subject } from 'rxjs';
 import { WebsocketService } from './websocket.service';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 const CHAT_URL = 'ws://echo.websocket.org/';
 
@@ -16,29 +15,17 @@ export class ChatService {
     public messages: Subject<Message>;
 
     constructor(wsService: WebsocketService) {
-        this.messages = wsService.connect(CHAT_URL).subscribe(
-          pipe(map(response: MessageEvent): Message => {
-              const data = JSON.parse(response.data);
+        this.messages = wsService.connect(CHAT_URL).pipe(
+            map(
+                (response: MessageEvent): Message => {
+                    const data = JSON.parse(response.data);
 
-              return {
-                author: data.author,
-                message: data.message,
-              };
-            }));
-
-
-
-        // )
-        //
-        //   .map(
-        //     (response: MessageEvent): Message => {
-        //         const data = JSON.parse(response.data);
-        //
-        //         return {
-        //             author: data.author,
-        //             message: data.message,
-        //         };
-        //     }
-        // );
+                    return {
+                        author: data.author,
+                        message: data.message,
+                    };
+                }
+            )
+        ) as Subject<Message>;
     }
 }
